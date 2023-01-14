ab28c8496fcb5cf093927d907ef4481a0e238198
jonghunbaek <qor6028@naver.com>
2023-01-14 13:52:16 +0900

    feat : checkBall method
    - this method check wheter the char is ball or not

--- src/main/java/baseball/CheckBall.java
+++ src/main/java/baseball/CheckBall.java
@@ -2,7 +2,7 @@ package baseball;
 
 public class CheckBall {
 
-	public int checkStrike(int comChar, int userChar, int i) {
+	public int checkStrike(int comChar, int userChar, int idx) {
 		if (comChar == userChar) {
 			return 1;
 		}
@@ -16,6 +16,15 @@ public class CheckBall {
 			count += checkStrike(computer.charAt(i), user.charAt(i), i);
 		}
 		return count;
+	}
+
+	public int checkBall(String userChar, String computer, int idx) {
+		int temp = computer.indexOf(userChar);
+		
+		if (temp == idx || temp == -1) {
+			return 0;
+		}	
+		return 1;
 	}
 
 
