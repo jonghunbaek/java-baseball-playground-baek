aa5aed47c495f91dd64a82ae3be62c6a9556ed28
jonghunbaek <qor6028@naver.com>
2023-01-17 18:21:36 +0900

    feat : checkThrowException
    - if text contains minus, it throws RuntimeException

--- src/main/java/string_calculator/StringCalculator.java
+++ src/main/java/string_calculator/StringCalculator.java
@@ -1,20 +1,52 @@
 package string_calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
 
-	public int textSplitBySeparator(String text) {
+	public int stringAddCalculator(String text) {
 		if (text == null || text == " " || text.isEmpty()) {
 			return 0;
 		}
 		if (text.length() == 1) {
 			return Integer.parseInt(text);
 		}
-		String[] temp = text.split(",|:");
+		
+		String[] temp = textSplitBySeparator(text);
+		
+		for (int i=0; i<temp.length; i++) {
+			checkMinus(temp[i]);
+		}
+		
 		int sum = 0;
 		for (int i=0; i<temp.length; i++) {
 			sum += Integer.parseInt(temp[i]);
 		}
 		return sum;
+	}
+
+	private void checkMinus(String temp) {
+		if (Integer.parseInt(temp) < 0) {
+			checkThrowException();
+		}
+	}
+
+	public String[] textSplitBySeparator(String text) {
+		Matcher mc = Pattern.compile("//(.)\n(.*)").matcher(text);
+		String[] temp;
+		
+		if (mc.find()) {
+			String customDelimeter = mc.group(1);
+			temp = mc.group(2).split(customDelimeter);
+			return temp;
+		}
+		
+		return text.split(",|:");
+	}
+
+	public void checkThrowException() {
+		throw new RuntimeException("숫자는 양의 정수만 가능합니다.");
 	}
 
 }
--- src/test/java/string_calculator_test/StringCalculatorTest.java
+++ src/test/java/string_calculator_test/StringCalculatorTest.java
@@ -22,19 +22,38 @@ public class StringCalculatorTest {
 		int result = 0;
 		String test1 = null;
 		String test2 = " ";
-		assertThat(result).isEqualTo(stringCalculator.textSplitBySeparator(test1));
-		assertThat(result).isEqualTo(stringCalculator.textSplitBySeparator(test2));
+		assertThat(result).isEqualTo(stringCalculator.stringAddCalculator(test1));
+		assertThat(result).isEqualTo(stringCalculator.stringAddCalculator(test2));
 	}
 	
 	@Test
 	@DisplayName("입력 문자 구분자로 나눈 후 덧셈")
-	public void textSplitBySeparator() {
+	public void stringAddCalculator() {
 		int result1 = 6;
 		int result2 = 2;
+		int result3 = 3;
 		String test1 = "1,2:3";
 		String test2 = "2";
+		String test3 = "1,2";
 		
-		assertThat(result1).isEqualTo(stringCalculator.textSplitBySeparator(test1));
-		assertThat(result2).isEqualTo(stringCalculator.textSplitBySeparator(test2));
+		assertThat(result1).isEqualTo(stringCalculator.stringAddCalculator(test1));
+		assertThat(result2).isEqualTo(stringCalculator.stringAddCalculator(test2));
+		assertThat(result3).isEqualTo(stringCalculator.stringAddCalculator(test3));
+	}
+	
+	@Test
+	@DisplayName("커스텀 구분자 지정 후 덧셈")
+	public void textSplitBySeparator() {
+		String[] result = {"1", "2", "3"};
+		String test = "//;\n1;2;3";
+		
+		assertThat(result).isEqualTo(stringCalculator.textSplitBySeparator(test));
+	}
+	
+	@Test
+	@DisplayName("음수, 숫자가 아닌 값 입력시 예외 발생")
+	public void checkThrowException() {
+		assertThatThrownBy(() -> stringCalculator.stringAddCalculator("-1,2,3"))
+								.isInstanceOf(RuntimeException.class);
 	}
 }
