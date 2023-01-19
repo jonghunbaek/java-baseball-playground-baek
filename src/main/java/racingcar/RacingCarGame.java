0c2b469244707a7bdbfcb97511134be7a99523f6
jonghunbaek <qor6028@naver.com>
2023-01-19 15:01:31 +0900

    feat : racingStart method
    - this make racingGame start

--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -1,6 +1,34 @@
 package racingcar;
 
 public class RacingCarGame {
+	
+	RacingCarUtil util = new RacingCarUtil();
+	
+	public void racingStart(String carsBefore, int times) {
+		String[] carsAfter = util.carTextSplit(carsBefore);
+		Car[] cars = new Car[carsAfter.length];
+		int[] ranNos = new int[carsAfter.length];
+		
+		for (int i=0; i<cars.length; i++) {
+			cars[i] = new Car(carsAfter[i], 0);
+		}
+
+		for (int i=0; i<times; i++) {
+			Car[] temp = checkMovingCar(cars, ranNos);
+			for (int j=0; j<temp.length; j++) {
+				System.out.println(temp[j].getCarName() + " : " + util.positionToString(temp[j].getPosition()));
+			}
+			System.out.println();
+		}
+	}
+
+	private Car[] checkMovingCar(Car[] cars, int[] ranNos) {
+		for (int i=0; i<ranNos.length; i++) {
+			ranNos[i] = createRandomNumber();
+			cars[i] = moveForwardCar(cars[i], ranNos[i]);
+		}
+		return cars;
+	}
 
 	public Car moveForwardCar(Car car, int randomNo) {
 		if (randomNo > 3) {
