9dd95ac54e39dd6bbd114017f81e84d920b8dd67
jonghunbaek <qor6028@naver.com>
2023-01-19 13:48:01 +0900

    fix : Car class constructor
    - 매개변수에 위치값 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -5,11 +5,13 @@ public class Car {
 	private String carName;
 	private int position;
 	
-	public Car (String carName) {
+	public Car (String carName, int position) {
 		if (carName.length() > 5) {
 			throw new RuntimeException("자동차의 이름은 5글자 이하여야 합니다.");
 		}
 		this.carName = carName;
+		this.position = position;
 	}
+	
 
 }
