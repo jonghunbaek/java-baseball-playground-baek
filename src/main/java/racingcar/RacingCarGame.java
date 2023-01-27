c2d851cabdb2f351fef0fb946f0a94f7fe494ca3
jonghunbaek <qor6028@naver.com>
2023-01-27 15:41:06 +0900

    feat : isSameCarName method
    - this method check whether one car name is same with other car name

--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -1,9 +1,18 @@
 package racingcar;
 
+import java.util.function.IntPredicate;
+
 public class RacingCarGame {
 
 	public String[] splitCarNames(String test) {
 		return test.split(",");
+	}
+
+	public boolean isSameCarName(Car test1, Car test2) {
+		if (test1.name.equals(test2.name)) {
+			return false;
+		}
+		return true;
 	}
 
 }
--- src/test/java/racingcar_test/RacingCarGameTest.java
+++ src/test/java/racingcar_test/RacingCarGameTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import racingcar.Car;
 import racingcar.RacingCarGame;
 
 public class RacingCarGameTest {
@@ -18,10 +19,18 @@ public class RacingCarGameTest {
 	}
 
 	@Test
-	@DisplayName("입려된 자동차 이름 쉼표로 분리 테스트")
+	@DisplayName("입력된 자동차 이름 쉼표로 분리 테스트")
 	public void splitCarNames() {
 		String test = "car1,car2,car3";
 		String[] result = {"car1","car2","car3"};
 		assertThat(result).isEqualTo(racingCarGame.splitCarNames(test));
+	}
+	
+	@Test
+	@DisplayName("입력된 자동차 이름 동일여부 테스트")
+	public void isSameCarName() {
+		Car test1 = new Car("car1", 0);
+		Car test2 = new Car("car1", 0);
+		assertThat(racingCarGame.isSameCarName(test1, test2)).isFalse();
 	}
 }
